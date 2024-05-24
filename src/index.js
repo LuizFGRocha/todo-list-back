985aa04dafd5966dc276591bc61730b16e47944f
Luiz Fernando <luizfgrocha04@gmail.com>
2024-05-23 21:45:48 -0300

    Adicionada conexão ao BD no index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import 'dotenv/config'
+import 'dotenv/config';
+import { server } from './server/server.js';
+import mongoose from 'mongoose';
+
+mongoose.connect(String(process.env.MONGODB_URI), { dbName: 'todo_list' })
+  .then(async () => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(() => {
+    console.log("Error connecting to MongoDB");
+  })
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
