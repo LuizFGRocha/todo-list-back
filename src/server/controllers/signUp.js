7327e97a1df46a9c4529fcd2580c12b0e41f1a0c
Luiz Fernando <luizfgrocha04@gmail.com>
2024-05-27 00:49:31 -0300

    Adicionadas intruções de uso em tarefa inicial.

--- src/server/controllers/signUp.js
+++ src/server/controllers/signUp.js
@@ -1,6 +1,43 @@
 import { StatusCodes } from "http-status-codes";
 import { User } from "../models/user.js";
 import bcrypt from "bcryptjs";
+import { TaskList } from "../models/taskList.js";
+import { Task } from "../models/task.js";
+
+const criarTaskListTutorial = async (user) => {
+const tutorialTaskList = new TaskList({
+  name: "Bem vindo ao To Do List!",
+  description: "Essa é a sua primeira task list. Você pode criar quantas quiser!",
+  owner: user._id,
+});
+const task1 = new Task({
+  title: "Clique no botão no canto inferior direito da tela inicial para criar uma tarefa!",
+  taskList: tutorialTaskList._id,
+});
+const task2 = new Task({
+  title: "Clique na lixeira para deletar a task list!",
+  taskList: tutorialTaskList._id,
+});
+const task3 = new Task({
+  title: "Clique no lápis para editar a task list!",
+  taskList: tutorialTaskList._id,
+});
+const task4 = new Task({
+  title: "Todos os campos são editáveis, e você pode marcar a task como concluída!",
+  taskList: tutorialTaskList._id,
+});
+const task5 = new Task({
+  title: "No modo de edição, clique na lixeira ao lado para deletar a task!",
+  taskList: tutorialTaskList._id,
+});
+const task6 = new Task({
+  title: "No modo de edição, clique em 'Salvar' para salvar as alterações!",
+  taskList: tutorialTaskList._id,
+});
+tutorialTaskList.tasks.push(task1, task2, task3, task4, task5, task6);
+await tutorialTaskList.save();
+task1.save(); task2.save(); task3.save(); task4.save(); task5.save(); task6.save();
+}
 
 export const signUp = async (req, res) => {
   try {
@@ -8,10 +45,8 @@ export const signUp = async (req, res) => {
       username: req.body.username,
       email: req.body.email,
       password: bcrypt.hashSync(req.body.password),
-      birthDate: req.body.birthDate,
-      gender: req.body.gender,
-      genres: req.body.genres,
     });
+    await criarTaskListTutorial(user);
     await user.save();
     return res.status(StatusCodes.CREATED).json({
       message: "Signup successful",
