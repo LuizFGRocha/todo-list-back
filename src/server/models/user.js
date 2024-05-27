2da8663229a7749ccdac654f75ec38b263acd6dd
Luiz Fernando <luizfgrocha04@gmail.com>
2024-05-27 14:41:10 -0300

    Removido campo não utilizado da model

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -4,7 +4,6 @@ const UserSchema = new mongoose.Schema({
   username: {type: String, required: true, unique: true}, // Nome de usuário
   email: {type: String, required: true, unique: true}, // Email
   password: {type: String, required: true}, // Senha
-  profilePicturePath: {type: String, default: 'default.jpg', required: true}, // Caminho da foto de perfil
 });
 
 export const User = mongoose.model('User', UserSchema);
