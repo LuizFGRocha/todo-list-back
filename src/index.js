be351f23de128bd67a0f73554934a68e9958fa94
Luiz Fernando <luizfgrocha04@gmail.com>
2024-09-08 16:20:05 -0300

    Adicionadas alterações para executar localmente

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import 'dotenv/config';
 import { server } from './server/server.js';
 import mongoose from 'mongoose';
+import { initializeDatabase } from '../tests/dbHandler.js';
 
-mongoose.connect(String(process.env.MONGODB_URI), { dbName: 'todo_list' })
-  .then(async () => {
-    console.log("Connected to MongoDB");
-  })
-  .catch(() => {
-    console.log("Error connecting to MongoDB");
-  })
+let dbHandler;
+
+if (process.env.NODE_ENV === 'development') {
+  dbHandler = await initializeDatabase();
+  dbHandler.connect().then(() => {
+    console.log("Connected to MongoDB memory server!");
+  }).catch(() => {
+    console.log("Error connecting to MongoDB memory server!");
+  });
+
+} else if (process.env.NODE_ENV === 'production') {
+  
+  mongoose.connect(String(process.env.MONGODB_URI), { dbName: 'todo_list' })
+    .then(async () => {
+      console.log("Connected to MongoDB");
+    })
+    .catch(() => {
+      console.log("Error connecting to MongoDB");
+    });
+} else {
+  console.log("Invalid NODE_ENV");
+  process.exit(1);
+}
 
 server.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
